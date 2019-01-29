ea872802ad85a50c7da0ecf8e5f21b090ca42f0b
Ariel Brewington <ArielBrewington@outlook.com>
2019-01-29 16:32:52 -0500

    Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,7 +6,31 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+                int[] elements = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+                foreach (int i in elements)
+
+                {
+
+                    Console.WriteLine("Element Value = " + i);
+
+                }
+
+                Console.WriteLine("__________________________________");
+                Console.WriteLine("");
+
+
+                Console.WriteLine("Press any key to exit the program.");
+                Console.ReadKey(true);
+
+            }
+            catch//end of catch
+
+            {
+            }
         }
     }
 }
